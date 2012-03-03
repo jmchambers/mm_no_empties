d2f6fdb3d2218b3cad0c909c3d8739d9ddcfaa64
Jonathan Chambers <j.chambers@gmx.net>
2012-03-03 03:33:17 +0000

    relaxed dependencies to enable use of latest mm, and removed InstanceMethods module to reflect changes in ActiveSupport::Concern

--- lib/mm_no_empties.rb
+++ lib/mm_no_empties.rb
@@ -5,47 +5,45 @@ require 'mongo_mapper/plugins/keys/key'
 module MmNoEmpties
   
   extend ActiveSupport::Concern
-  
-  module InstanceMethods
-    
-    def attributes(include_all = false)
       
-      HashWithIndifferentAccess.new.tap do |attrs|
-        persistable_keys = if include_all
-          keys
-        else
-          keys.select do |name, key|
-            val = self[key.name]
-            #key.type == ObjectId or   # this is in the original :attributes implementation, but is seems safe to remove it as 'belongs_to: nil' if it's missing
-            not (val.nil? or (val.respond_to?(:empty?) and val.empty?))
-          end
+  def attributes(include_all = false)
+    
+    HashWithIndifferentAccess.new.tap do |attrs|
+      persistable_keys = if include_all
+        keys
+      else
+        keys.select do |name, key|
+          val = self[key.name]
+          #key.type == ObjectId or   # this is in the original :attributes implementation, but is seems safe to remove it as 'belongs_to: nil' if it's missing
+          not (val.nil? or (val.respond_to?(:empty?) and val.empty?))
         end
-        
-        persistable_keys.each do |name, key|
-          value = key.set(self[key.name])
-          attrs[name] = value
-        end
+      end
+      
+      persistable_keys.each do |name, key|
+        value = key.set(self[key.name])
+        attrs[name] = value
+      end
 
-        embedded_associations.each do |association|
-          if documents = instance_variable_get(association.ivar)
-            
-            val = if association.is_a?(MongoMapper::Plugins::Associations::OneAssociation)
-              documents.to_mongo
-            else
-              documents.map { |document| document.to_mongo }
-            end
-              
-            if include_all
-              attrs[association.name] = val
-            else
-              attrs[association.name] = val unless val.nil? or (val.is_a?(Array) and val.empty?)
-            end
-            
+      embedded_associations.each do |association|
+        if documents = instance_variable_get(association.ivar)
+          
+          val = if association.is_a?(MongoMapper::Plugins::Associations::OneAssociation)
+            documents.to_mongo
+          else
+            documents.map { |document| document.to_mongo }
           end
+            
+          if include_all
+            attrs[association.name] = val
+          else
+            attrs[association.name] = val unless val.nil? or (val.is_a?(Array) and val.empty?)
+          end
+          
         end
       end
     end
-    alias :to_mongo :attributes
   end
+  alias :to_mongo :attributes
+
   
 end
